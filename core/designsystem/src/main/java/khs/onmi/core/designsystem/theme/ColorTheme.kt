e5660443b331a92342b39b16016501c638b9cf1c
khs3994 <z01039948003@gmail.com>
2023-10-25 10:44:09 +0900

    :lipstick: :: change the way white wheme color and dark theme color are implemented separately through abstract class

---
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/theme/ColorTheme.kt
@@ -0,0 +1,23 @@
+package khs.onmi.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+abstract class ColorTheme {
+    internal val Point = Color(0xFFFF5959)
+    internal val Success = Color(0xFF4B79EF)
+    internal val AbsoluteWhite = Color(0xFFFFFFFF)
+    internal val AbsoluteBlack = Color(0xFF000000)
+
+    //White Theme Color
+    abstract val TextPrimary: Color
+    abstract val TextSecondary: Color
+    abstract val UnselectedPrimary: Color
+    abstract val UnselectedSecondary: Color
+    abstract val CardBackground: Color
+    abstract val CardBackgroundSecondary: Color
+    abstract val BackgroundMain: Color
+    abstract val BackgroundSecondary: Color
+    abstract val White: Color
+    abstract val Black: Color
+    abstract val LightBox: Color
+}
---
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/theme/color/DarkThemeColor.kt
@@ -0,0 +1,18 @@
+package khs.onmi.core.designsystem.theme.color
+
+import androidx.compose.ui.graphics.Color
+import khs.onmi.core.designsystem.theme.ColorTheme
+
+object DarkThemeColor : ColorTheme() {
+    override val TextPrimary = Color(0xFFD8D8DF)
+    override val TextSecondary = Color(0xFF84849A)
+    override val UnselectedPrimary = Color(0xFF5C5C70)
+    override val UnselectedSecondary = Color(0xFF272730)
+    override val CardBackground = Color(0xFF18181D)
+    override val CardBackgroundSecondary = Color(0xFF18181D)
+    override val BackgroundMain = Color(0xFF000000)
+    override val BackgroundSecondary = Color(0xFF000000)
+    override val White = Color(0xFF000000)
+    override val Black = Color(0xFFFFFFFF)
+    override val LightBox = Color(0x33FFFFFF)
+}
