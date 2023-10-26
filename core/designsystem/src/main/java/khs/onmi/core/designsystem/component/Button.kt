704e0a54732003779b7a213f8661e0c6d43a7e2d
khs3994 <z01039948003@gmail.com>
2023-10-26 17:59:40 +0900

    :lipstick: :: make button component

---
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/component/Button.kt
@@ -0,0 +1,52 @@
+package khs.onmi.core.designsystem.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import khs.onmi.core.designsystem.theme.ONMITheme
+
+@Composable
+fun ONMIButton(
+    text: String,
+    isEnable: Boolean,
+    onClick: () -> Unit,
+) {
+    ONMITheme { color, typography ->
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(52.dp)
+                .padding(horizontal = 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = color.White,
+                disabledContentColor = color.UnselectedPrimary,
+                containerColor = color.Black,
+                disabledContainerColor = color.UnselectedSecondary
+            ),
+            shape = RoundedCornerShape(8.dp),
+            onClick = onClick,
+            enabled = isEnable
+        ) {
+            Text(
+                text = text,
+                style = typography.Headline4
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ONMIButtonPre() {
+    ONMIButton(text = "Text", isEnable = false) {
+
+    }
+}
