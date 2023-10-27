153485008c4b591c6de90d0fdebae90bd1c4b3e1
khs3994 <z01039948003@gmail.com>
2023-10-27 11:38:01 +0900

    :lipstick: :: modify code to change color based on theme

--- core/designsystem/src/main/java/khs/onmi/core/designsystem/icon/LargeIcon.kt
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/icon/LargeIcon.kt
@@ -1,21 +1,45 @@
 package khs.onmi.core.designsystem.icon
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import khs.onmi.core.designsystem.R
 import khs.onmi.core.designsystem.theme.ONMITheme
+
+@Composable
+fun BasicLargeIcon(
+    id: Int,
+    contentDescription: String,
+    modifier: Modifier,
+) {
+    ONMITheme { color, _ ->
+        Icon(
+            painter = painterResource(id = id),
+            contentDescription = contentDescription,
+            tint = color.Black,
+            modifier = modifier
+        )
+    }
+}
 
 @Composable
 fun NotificationsIcon(
     modifier: Modifier = Modifier,
 ) {
-    Image(
-        painter = painterResource(id = R.drawable.ic_notifications),
+    BasicLargeIcon(
+        id = R.drawable.ic_notifications,
         contentDescription = "Large Notifications Icon",
         modifier = modifier
     )
@@ -25,19 +49,36 @@ fun NotificationsIcon(
 fun NotificationsFillIcon(
     modifier: Modifier = Modifier,
 ) {
-    Image(
-        painter = painterResource(id = R.drawable.ic_notifications_fill),
-        contentDescription = "Large Notifications Fill Icon",
-        modifier = modifier
-    )
+    ONMITheme { color, _ ->
+        Box(modifier = Modifier.size(32.dp)) {
+            BasicLargeIcon(
+                id = R.drawable.ic_notifications_fill,
+                contentDescription = "Large Notifications Fill Icon",
+                modifier = modifier
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 4.5.dp, end = 7.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(6.dp)
+                        .clip(CircleShape)
+                        .background(color.Point)
+                        .align(Alignment.TopEnd)
+                )
+            }
+        }
+    }
 }
 
 @Composable
 fun SettingIcon(
     modifier: Modifier = Modifier,
 ) {
-    Image(
-        painter = painterResource(id = R.drawable.ic_setting),
+    BasicLargeIcon(
+        id = R.drawable.ic_setting,
         contentDescription = "Large Setting Icon",
         modifier = modifier
     )
@@ -47,8 +88,8 @@ fun SettingIcon(
 fun AllergiesIcon(
     modifier: Modifier = Modifier,
 ) {
-    Image(
-        painter = painterResource(id = R.drawable.ic_large_allergies),
+    BasicLargeIcon(
+        id = R.drawable.ic_large_allergies,
         contentDescription = "Large allergies Icon",
         modifier = modifier
     )
@@ -58,8 +99,8 @@ fun AllergiesIcon(
 fun GithubIcon(
     modifier: Modifier = Modifier,
 ) {
-    Image(
-        painter = painterResource(id = R.drawable.ic_github),
+    BasicLargeIcon(
+        id = R.drawable.ic_github,
         contentDescription = "Large Github Icon",
         modifier = modifier
     )
@@ -69,8 +110,8 @@ fun GithubIcon(
 fun EmailIcon(
     modifier: Modifier = Modifier,
 ) {
-    Image(
-        painter = painterResource(id = R.drawable.ic_email),
+    BasicLargeIcon(
+        id = R.drawable.ic_email,
         contentDescription = "Large Email Icon",
         modifier = modifier
     )
@@ -80,7 +121,7 @@ fun EmailIcon(
 @Composable
 fun LargeIconsPre() {
     ONMITheme { color, _ ->
-        Column(modifier = Modifier.background(color.AbsoluteWhite)) {
+        Column(modifier = Modifier.background(color.White)) {
             NotificationsIcon()
             NotificationsFillIcon()
             SettingIcon()
