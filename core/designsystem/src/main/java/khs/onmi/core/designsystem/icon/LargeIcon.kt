56e4a4dcb8f5af478ec014a489526ceb4efbab27
khs3994 <z01039948003@gmail.com>
2023-10-27 11:40:30 +0900

    :lipstick: :: change file location to use baseicon with other icon

--- core/designsystem/src/main/java/khs/onmi/core/designsystem/icon/LargeIcon.kt
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/icon/LargeIcon.kt
@@ -7,38 +7,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import khs.onmi.core.designsystem.R
 import khs.onmi.core.designsystem.theme.ONMITheme
 
 @Composable
-fun BasicLargeIcon(
-    id: Int,
-    contentDescription: String,
-    modifier: Modifier,
-) {
-    ONMITheme { color, _ ->
-        Icon(
-            painter = painterResource(id = id),
-            contentDescription = contentDescription,
-            tint = color.Black,
-            modifier = modifier
-        )
-    }
-}
-
-@Composable
 fun NotificationsIcon(
     modifier: Modifier = Modifier,
 ) {
-    BasicLargeIcon(
+    BasicIcon(
         id = R.drawable.ic_notifications,
         contentDescription = "Large Notifications Icon",
         modifier = modifier
@@ -51,7 +33,7 @@ fun NotificationsFillIcon(
 ) {
     ONMITheme { color, _ ->
         Box(modifier = Modifier.size(32.dp)) {
-            BasicLargeIcon(
+            BasicIcon(
                 id = R.drawable.ic_notifications_fill,
                 contentDescription = "Large Notifications Fill Icon",
                 modifier = modifier
@@ -77,7 +59,7 @@ fun NotificationsFillIcon(
 fun SettingIcon(
     modifier: Modifier = Modifier,
 ) {
-    BasicLargeIcon(
+    BasicIcon(
         id = R.drawable.ic_setting,
         contentDescription = "Large Setting Icon",
         modifier = modifier
@@ -88,7 +70,7 @@ fun SettingIcon(
 fun AllergiesIcon(
     modifier: Modifier = Modifier,
 ) {
-    BasicLargeIcon(
+    BasicIcon(
         id = R.drawable.ic_large_allergies,
         contentDescription = "Large allergies Icon",
         modifier = modifier
@@ -99,7 +81,7 @@ fun AllergiesIcon(
 fun GithubIcon(
     modifier: Modifier = Modifier,
 ) {
-    BasicLargeIcon(
+    BasicIcon(
         id = R.drawable.ic_github,
         contentDescription = "Large Github Icon",
         modifier = modifier
@@ -110,7 +92,7 @@ fun GithubIcon(
 fun EmailIcon(
     modifier: Modifier = Modifier,
 ) {
-    BasicLargeIcon(
+    BasicIcon(
         id = R.drawable.ic_email,
         contentDescription = "Large Email Icon",
         modifier = modifier
