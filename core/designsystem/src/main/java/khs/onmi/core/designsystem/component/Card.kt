c687e95ac8a09191510091f483e2cdc1240d3b64
khs3994 <z01039948003@gmail.com>
2023-11-06 23:17:49 +0900

    :lipstick: :: change how the card ui is configured

--- core/designsystem/src/main/java/khs/onmi/core/designsystem/component/Card.kt
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/component/Card.kt
@@ -1,6 +1,7 @@
 package khs.onmi.core.designsystem.component
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,8 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,13 +24,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import khs.onmi.core.designsystem.icon.AllergiesEggIcon
 import khs.onmi.core.designsystem.modifier.addBounceEffect
+import khs.onmi.core.designsystem.modifier.onmiClickable
 import khs.onmi.core.designsystem.theme.ONMITheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AllergiesCard(
     modifier: Modifier = Modifier,
@@ -42,46 +43,49 @@ fun AllergiesCard(
     onItemClick: () -> Unit,
 ) {
     ONMITheme { color, typography ->
-        Card(
-            modifier = modifier.addBounceEffect(),
-            shape = RoundedCornerShape(16.dp),
-            border = BorderStroke(2.dp, color.Black).takeIf { isSelected },
-            colors = CardDefaults.cardColors(containerColor = color.CardBackgroundSecondary),
-            onClick = onItemClick
+        Box(
+            modifier = modifier
+                .addBounceEffect()
+                .onmiClickable(rippleEnabled = false, onClick = onItemClick)
+                .clip(RoundedCornerShape(16.dp))
+                .background(color.CardBackgroundSecondary)
+                .border(
+                    2.dp,
+                    if (isSelected) color.Black else Color.Transparent,
+                    RoundedCornerShape(16.dp)
+                )
         ) {
-            Box(modifier = Modifier.fillMaxSize()) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .align(Alignment.CenterStart),
-                    verticalArrangement = Arrangement.Center
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .align(Alignment.CenterStart),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(start = 20.dp)
                 ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.padding(start = 20.dp)
-                    ) {
-                        allergyIcon()
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(
-                            text = allergyName,
-                            style = typography.Body3,
-                            color = if (isSelected) color.TextPrimary else color.UnselectedPrimary
-                        )
-                    }
-                }
-                Box(
-                    modifier = Modifier
-                        .size(24.dp)
-                        .align(Alignment.TopEnd)
-                        .offset(x = (-8).dp, y = 8.dp)
-                ) {
+                    allergyIcon()
+                    Spacer(modifier = Modifier.width(8.dp))
                     Text(
-                        text = itemNumber.toString(),
+                        text = allergyName,
                         style = typography.Body3,
-                        color = if (isSelected) color.TextPrimary else color.UnselectedPrimary,
-                        modifier = Modifier.align(Alignment.Center)
+                        color = if (isSelected) color.TextPrimary else color.UnselectedPrimary
                     )
                 }
+            }
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(Alignment.TopEnd)
+                    .offset(x = (-8).dp, y = 8.dp)
+            ) {
+                Text(
+                    text = itemNumber.toString(),
+                    style = typography.Body3,
+                    color = if (isSelected) color.TextPrimary else color.UnselectedPrimary,
+                    modifier = Modifier.align(Alignment.Center)
+                )
             }
         }
     }
