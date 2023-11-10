a8f05223ce8fcc7a1e2e3f5aa1180c93dbde4f55
khs3994 <z01039948003@gmail.com>
2023-11-10 22:50:25 +0900

    :lipstick: :: add keyboard action and imeAction

--- core/designsystem/src/main/java/khs/onmi/core/designsystem/component/TextField.kt
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/component/TextField.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -14,9 +16,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import khs.onmi.core.designsystem.icon.XMarkCircleFillIcon
@@ -28,6 +35,9 @@ fun ONMITextField(
     modifier: Modifier = Modifier,
     value: String,
     placeHolderText: String,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
     onValueChange: (String) -> Unit,
     onTrailingIconClick: () -> Unit,
 ) {
@@ -71,15 +81,22 @@ fun ONMITextField(
                     }
                 }
             },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction
+            ),
+            keyboardActions = keyboardActions,
             maxLines = 1,
             shape = RoundedCornerShape(8.dp),
         )
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun ONMITextFieldPre() {
+    val kc = LocalSoftwareKeyboardController.current
     var text by remember {
         mutableStateOf("")
     }
