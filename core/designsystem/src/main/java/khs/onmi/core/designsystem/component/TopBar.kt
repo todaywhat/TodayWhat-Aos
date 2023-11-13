580454e4f76cf3b85a1526dd3fc7255e631d862d
khs3994 <z01039948003@gmail.com>
2023-11-14 00:38:05 +0900

    :lipstick: :: add title and right icons

--- core/designsystem/src/main/java/khs/onmi/core/designsystem/component/TopBar.kt
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/component/TopBar.kt
@@ -8,10 +8,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import khs.onmi.core.designsystem.icon.ArrowBackIcon
 import khs.onmi.core.designsystem.icon.NotificationsIcon
 import khs.onmi.core.designsystem.theme.ONMITheme
 import khs.onmi.core.designsystem.utils.WrappedIconButton
