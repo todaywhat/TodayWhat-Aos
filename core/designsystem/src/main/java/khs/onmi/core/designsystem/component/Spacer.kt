5a2eab04e1c3521972f1b8f766388b0731d01c9f
khs3994 <z01039948003@gmail.com>
2023-11-15 00:22:08 +0900

    :memo: :: rename functions

--- core/designsystem/src/main/java/khs/onmi/core/designsystem/component/Spacer.kt
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/component/Spacer.kt
@@ -4,26 +4,27 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 
 @Composable
-fun ColumnSpacerWithDp(dp: Dp) {
+fun ColumnSpacer(dp: Dp) {
     Spacer(modifier = Modifier.height(dp))
 }
 
 @Composable
-fun ColumnSpacerWithPercent(percent: Float) {
+fun ColumnSpacer(percent: Float) {
     Spacer(modifier = Modifier.fillMaxHeight(percent))
 }
 
 @Composable
-fun RowSpacerWithDp(dp: Dp) {
+fun RowSpacer(dp: Dp) {
     Spacer(modifier = Modifier.width(dp))
 }
 
 @Composable
-fun RowSpacerWithPercent(percent: Float) {
+fun RowSpacer(percent: Float) {
     Spacer(modifier = Modifier.fillMaxWidth(percent))
 }
