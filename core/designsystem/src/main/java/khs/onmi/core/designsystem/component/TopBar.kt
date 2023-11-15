492800e9b88b3796bef15b2781eaf738146e79cd
khs3994 <z01039948003@gmail.com>
2023-11-15 23:54:30 +0900

    :lipstick: :: add title and right icons

--- core/designsystem/src/main/java/khs/onmi/core/designsystem/component/TopBar.kt
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/component/TopBar.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,23 +12,49 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import khs.onmi.core.designsystem.icon.ArrowBackIcon
-import khs.onmi.core.designsystem.modifier.onmiClickable
+import khs.onmi.core.designsystem.icon.NotificationsIcon
 import khs.onmi.core.designsystem.theme.ONMITheme
+import khs.onmi.core.designsystem.utils.WrappedIconButton
 
 @Composable
 fun TopNavigationBar(
-    insideLeftIcon: (@Composable () -> Unit)? = null,
+    title: String = "",
     onInSideLeftIconClick: (() -> Unit)? = null,
+    onOutSideLeftIconClick: (() -> Unit)? = null,
+    onInSideRightIconClick: (() -> Unit)? = null,
+    onOutSideRightIconClick: (() -> Unit)? = null,
+    outsideLeftIcon: (@Composable () -> Unit)? = null,
+    insideLeftIcon: (@Composable () -> Unit)? = null,
+    outsideRightIcon: (@Composable () -> Unit)? = null,
+    insideRightIcon: (@Composable () -> Unit)? = null,
 ) {
-    ONMITheme { color, _ ->
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
+    ONMITheme { color, typography ->
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
         ) {
-            insideLeftIcon?.let {
-                IconButton(onClick = onInSideLeftIconClick!!) {
-                    insideLeftIcon()
-                }
+            Row(
+                modifier = Modifier.align(Alignment.CenterStart),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                WrappedIconButton(onClick = onOutSideLeftIconClick, icon = outsideLeftIcon)
+                RowSpacer(dp = 16.dp)
+                WrappedIconButton(onClick = onInSideLeftIconClick, icon = insideLeftIcon)
+            }
+            Text(
+                text = title,
+                modifier = Modifier.align(Alignment.Center),
+                style = typography.Headline4,
+                color = color.Black
+            )
+            Row(
+                modifier = Modifier.align(Alignment.CenterEnd),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                WrappedIconButton(onClick = onInSideRightIconClick, icon = insideRightIcon)
+                RowSpacer(dp = 16.dp)
+                WrappedIconButton(onClick = onOutSideRightIconClick, icon = outsideRightIcon)
             }
         }
     }
@@ -37,12 +63,25 @@ fun TopNavigationBar(
 @Preview
 @Composable
 fun TopNavigationBarPre() {
-    TopNavigationBar(
-        insideLeftIcon = {
-            ArrowBackIcon(tint = Color.Black)
-        },
-        onInSideLeftIconClick = {
-
-        }
-    )
+    ONMITheme { color, _ ->
+        TopNavigationBar(
+            title = "오늘뭐임탑바",
+            outsideLeftIcon = {
+                NotificationsIcon(tint = color.Black)
+            },
+            insideLeftIcon = {
+                NotificationsIcon(tint = color.Black)
+            },
+            outsideRightIcon = {
+                NotificationsIcon(tint = color.Black)
+            },
+            insideRightIcon = {
+                NotificationsIcon(tint = color.Black)
+            },
+            onOutSideLeftIconClick = {},
+            onInSideLeftIconClick = {},
+            onOutSideRightIconClick = {},
+            onInSideRightIconClick = {}
+        )
+    }
 }
