718eb24640a63c0d45bba667ff0d784d016699e9
khs3994 <z01039948003@gmail.com>
2023-11-15 18:41:35 +0900

    :memo: :: refactor click able onClick can be null

--- core/designsystem/src/main/java/khs/onmi/core/designsystem/component/TopBar.kt
+++ core/designsystem/src/main/java/khs/onmi/core/designsystem/component/TopBar.kt
@@ -1,11 +1,14 @@
 package khs.onmi.core.designsystem.component
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import khs.onmi.core.designsystem.icon.ArrowBackIcon
@@ -13,21 +16,20 @@ import khs.onmi.core.designsystem.modifier.onmiClickable
 import khs.onmi.core.designsystem.theme.ONMITheme
 
 @Composable
-fun TopNavigationBar(onBackButtonClick: () -> Unit) {
+fun TopNavigationBar(
+    insideLeftIcon: (@Composable () -> Unit)? = null,
+    onInSideLeftIconClick: (() -> Unit)? = null,
+) {
     ONMITheme { color, _ ->
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            ArrowBackIcon(
-                tint = color.Black,
-                modifier = Modifier
-                    .padding(start = 16.dp, top = 8.dp, bottom = 8.dp)
-                    .onmiClickable(
-                        onClick = onBackButtonClick,
-                        bounded = false
-                    )
-            )
+            insideLeftIcon?.let {
+                IconButton(onClick = onInSideLeftIconClick!!) {
+                    insideLeftIcon()
+                }
+            }
         }
     }
 }
@@ -35,7 +37,12 @@ fun TopNavigationBar(onBackButtonClick: () -> Unit) {
 @Preview
 @Composable
 fun TopNavigationBarPre() {
-    TopNavigationBar {
+    TopNavigationBar(
+        insideLeftIcon = {
+            ArrowBackIcon(tint = Color.Black)
+        },
+        onInSideLeftIconClick = {
 
-    }
+        }
+    )
 }
