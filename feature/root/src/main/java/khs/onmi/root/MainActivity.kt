ae9d515f35a81823aa2e91571c42984666cf80a0
khs3994 <z01039948003@gmail.com>
2023-11-27 23:26:35 +0900

    :lipstick: :: add root navhost & navroutes file

---
+++ feature/root/src/main/java/khs/onmi/root/MainActivity.kt
@@ -0,0 +1,29 @@
+package khs.onmi.root
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import khs.onmi.navigation.ONMINavRoutes
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+
+            NavHost(
+                navController = navController,
+                startDestination = ONMINavRoutes.ENTERINFOMATION
+            ) {
+                composable(
+                    route = ONMINavRoutes.ENTERINFOMATION
+                ) {
+
+                }
+            }
+        }
+    }
+}
