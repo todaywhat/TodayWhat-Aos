4517c061ea470ceb4b2689553b665f6a72a4c204
khs3994 <z01039948003@gmail.com>
2023-12-28 01:22:09 +0900

    :sparkles: add edu code & school code

--- domain/school/data/src/main/java/khs/onmi/school/data/dto/response/SearchSchoolByNameResponseDto.kt
+++ domain/school/data/src/main/java/khs/onmi/school/data/dto/response/SearchSchoolByNameResponseDto.kt
@@ -6,15 +6,23 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SearchSchoolByNameResponseDto(
+    @SerialName("ATPT_OFCDC_SC_CODE")
+    val educationCode: String,
+
+    @SerialName("SD_SCHUL_CODE")
+    val schoolCode: String,
+
     @SerialName("SCHUL_NM")
-    val schoolName: String = "",
+    val schoolName: String,
 
     @SerialName("ORG_RDNMA")
-    val schoolLocation: String = "",
+    val schoolLocation: String,
 )
 
 fun SearchSchoolByNameResponseDto.toModel(): SearchSchoolByNameResponseModel {
     return SearchSchoolByNameResponseModel(
+        educationCode = this.educationCode,
+        schoolCode = this.schoolCode,
         schoolName = this.schoolName,
         schoolLocation = this.schoolLocation
     )
