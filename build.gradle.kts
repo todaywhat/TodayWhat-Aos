dee728d54ebfdf87a6071fb194a0f996a7468a19
khs3994 <z01039948003@gmail.com>
2023-12-28 23:52:13 +0900

    :heavy_plus_sign: :: add room dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,5 @@ plugins {
     alias(libs.plugins.com.android.library) apply false
     alias(libs.plugins.hilt.plugin) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
