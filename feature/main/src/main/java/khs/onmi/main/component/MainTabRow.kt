ee36de7865602cf3867e355ee2ac5bb2f163dd7b
khs3994 <z01039948003@gmail.com>
2024-01-08 23:41:35 +0900

    :lipstick: :: add main page top components

---
+++ feature/main/src/main/java/khs/onmi/main/component/MainTabRow.kt
@@ -0,0 +1,59 @@
+package khs.onmi.main.component
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import khs.onmi.core.designsystem.theme.ONMITheme
+import khs.onmi.main.util.customTabIndicatorOffset
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MainTabRow(
+    tabs: List<String>,
+    pagerState: PagerState,
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    ONMITheme { color, typography ->
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    modifier = Modifier
+                        .customTabIndicatorOffset(
+                            tabPositions[pagerState.currentPage],
+                            146.dp
+                        ),
+                    color = color.Black // 색상 지정
+                )
+            },
+            containerColor = color.BackgroundMain
+        ) {
+            tabs.forEachIndexed { index, tab ->
+                Tab(
+                    text = {
+                        Text(
+                            text = tab,
+                            style = typography.Headline4,
+                            color = if (index == pagerState.currentPage) color.Black else color.UnselectedPrimary
+                        )
+                    },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
