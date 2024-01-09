525c2718bbd70be452e03393b53234fc61f8d85c
khs3994 <z01039948003@gmail.com>
2024-01-10 01:00:55 +0900

    :lipstick: :: add meals section component

--- feature/main/src/main/java/khs/onmi/main/screen/MainScreen.kt
+++ feature/main/src/main/java/khs/onmi/main/screen/MainScreen.kt
@@ -3,6 +3,7 @@ package khs.onmi.main.screen
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,6 +19,8 @@ import khs.onmi.core.designsystem.component.InfoCard
 import khs.onmi.core.designsystem.component.TopNavigationBar
 import khs.onmi.core.designsystem.theme.ONMITheme
 import khs.onmi.main.component.MainTabRow
+import khs.onmi.main.component.MealsSection
+import khs.onmi.main.component.TimeTableSection
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -30,7 +33,7 @@ fun MainScreen() {
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 16.dp)
+                        .padding(top = 16.dp)
                 ) {
                     TopNavigationBar(
                         leading = {
@@ -62,11 +65,31 @@ fun MainScreen() {
             containerColor = color.BackgroundMain
         ) { paddingValues ->
             HorizontalPager(
-                modifier = Modifier.padding(paddingValues),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
                 state = pagerState,
                 pageCount = 2
             ) { index ->
+                when (index) {
+                    0 -> MealsSection(
+                        breakfast = listOf("쌀밥"),
+                        lunch = null,
+                        dinner = null
+                    )
 
+                    1 -> TimeTableSection(
+                        timeTableList = listOf(
+                            Pair("1", "한국사"),
+                            Pair("2", "한국사"),
+                            Pair("3", "한국사"),
+                            Pair("4", "한국사"),
+                            Pair("5", "한국사"),
+                            Pair("6", "한국사"),
+                            Pair("7", "한국사"),
+                        )
+                    )
+                }
             }
         }
     }
