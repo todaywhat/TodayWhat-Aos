3293a5ff3cbcea2ef64d99950491ff8cf0fb5886
khs3994 <z01039948003@gmail.com>
2024-01-30 00:35:32 +0900

    :sparkles: :: add setting tab & policy component

---
+++ feature/setup/src/main/java/khs/onmi/setup/screen/MainScreen.kt
@@ -0,0 +1,75 @@
+package khs.onmi.setup.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import khs.onmi.core.designsystem.component.TopNavigationBar
+import khs.onmi.core.designsystem.icon.AllergiesIcon
+import khs.onmi.core.designsystem.icon.ArrowBackIcon
+import khs.onmi.core.designsystem.icon.RightArrowIcon
+import khs.onmi.core.designsystem.theme.ONMITheme
+import khs.onmi.core.designsystem.utils.WrappedIconButton
+import khs.onmi.setup.component.RoundedWhiteBox
+import khs.onmi.setup.component.SettingListComponent
+import khs.onmi.setup.model.SettingItemsData
+
+@Composable
+fun MainScreen() {
+    ONMITheme { color, typography ->
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            containerColor = color.BackgroundSecondary,
+            topBar = {
+                TopNavigationBar(
+                    leading = {
+                        WrappedIconButton(onClick = {}) {
+                            ArrowBackIcon(tint = color.Black)
+                        }
+                    }
+                )
+            },
+        ) {
+            Column(
+                modifier = Modifier.padding(
+                    top = it.calculateTopPadding().value.dp,
+                    start = 15.dp,
+                    end = 15.dp
+                )
+            ) {
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    text = "설정",
+                    style = typography.Headline1,
+                    color = color.Black
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                RoundedWhiteBox {
+                    SettingListComponent(
+                        items = listOf(
+                            SettingItemsData(
+                                trailing = { RightArrowIcon(tint = color.UnselectedPrimary) },
+                                text = "이용 약관",
+                                leading = { AllergiesIcon(tint = color.Black) },
+                                onClick = { /*TODO*/ }
+                            )
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPre() {
+    MainScreen()
+}
