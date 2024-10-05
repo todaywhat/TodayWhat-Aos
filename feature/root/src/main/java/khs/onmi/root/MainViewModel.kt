289755bc313b1a60f8ddd46ef6aff7c474743dd0
khs3994 <dev.hyunsss@gmail.com>
2024-10-05 22:15:49 +0900

    [Add] :: period값이 똑같은 아이템이 있다면 제거하는 로직 추가

--- feature/root/src/main/java/khs/onmi/root/MainViewModel.kt
+++ feature/root/src/main/java/khs/onmi/root/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.onmi.database.UserDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,7 +15,7 @@ class MainViewModel @Inject constructor(
 ) : ViewModel() {
     fun checkUserAlreadyEnteredInfo(action: (isEntered: Boolean) -> Unit) = viewModelScope.launch {
         kotlin.runCatching {
-            userDao.getUserInfo()
+            userDao.getUserInfo().first()
         }.onSuccess {
             action(it != null)
         }.onFailure {
