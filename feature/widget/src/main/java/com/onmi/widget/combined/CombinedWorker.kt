6ec02ada3803c5f2363893160b8c0b7860591dde
khs3994 <dev.hyunsss@gmail.com>
2024-11-13 23:20:50 +0900

    [Chore] :: CombinedWorker 에서 WidgetDataDisplayManager 사용하여 급식정보 노출하도록 수정

--- feature/widget/src/main/java/com/onmi/widget/combined/CombinedWorker.kt
+++ feature/widget/src/main/java/com/onmi/widget/combined/CombinedWorker.kt
@@ -14,15 +14,13 @@ import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import com.onmi.domain.usecase.meal.GetTodayMealsUseCase
 import com.onmi.domain.usecase.timetable.GetTodayTimeTableUseCase
-import com.onmi.widget.util.MealTime
+import com.onmi.widget.util.MealInfoState
 import com.onmi.widget.util.WidgetDataDisplayManager
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import khs.onmi.core.common.android.EventLogger
 import khs.onmi.core.common.android.WidgetFamily
 import khs.onmi.core.common.android.WidgetKind
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import java.time.Duration
 
 @HiltWorker
@@ -56,50 +54,29 @@ class CombinedWorker @AssistedInject constructor(
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    override suspend fun doWork(): Result = coroutineScope {
-        try {
-            val getTodayMealsRequest = async { getTodayMealsUseCase() }
-            val getTodayTimeTableRequest = async { getTodayTimeTableUseCase() }
+    override suspend fun doWork(): Result {
+        val todayTimeTable = getTodayTimeTableUseCase().getOrElse {
+            setWidgetState(CombinedInfo.Unavailable)
+            return Result.success()
+        }
 
-            val getTodayMealsResponse = getTodayMealsRequest.await()
-            val getTodayTimeTableResponse = getTodayTimeTableRequest.await()
+        val currentMealTime = WidgetDataDisplayManager.getCurrentMealTime()
+        val mealsInfo = WidgetDataDisplayManager.fetchMealInfo(
+            getTodayMealsUseCase = getTodayMealsUseCase,
+            requestedMealTime = currentMealTime
+        )
 
-            if (getTodayMealsResponse.isFailure || getTodayTimeTableResponse.isFailure) Result.failure()
-
-            val todayMeals =
-                getTodayMealsResponse.getOrNull() ?: return@coroutineScope Result.failure()
-            val todayTimeTable =
-                getTodayTimeTableResponse.getOrNull() ?: return@coroutineScope Result.failure()
-
-            WidgetDataDisplayManager.fetchMealInfo(
-                getTodayMealsUseCase = getTodayMealsUseCase,
-                requestedMealTime = WidgetDataDisplayManager.getCurrentMealTime()
-            )
-
-            setWidgetState(
+        setWidgetState(
+            if (mealsInfo is MealInfoState.Available) {
                 CombinedInfo.Available(
-                    mealTime = when (WidgetDataDisplayManager.getCurrentMealTime()) {
-                        MealTime.Morning -> "아침"
-
-                        MealTime.Lunch -> "점심"
-
-                        MealTime.Dinner -> "저녁"
-                    },
-                    mealList = when (WidgetDataDisplayManager.getCurrentMealTime()) {
-                        MealTime.Morning -> todayMeals.breakfast.first
-
-                        MealTime.Lunch -> todayMeals.lunch.first
-
-                        MealTime.Dinner -> todayMeals.dinner.first
-                    },
+                    mealTime = mealsInfo.mealTime,
+                    mealList = mealsInfo.mealList,
                     subjectList = todayTimeTable
                 )
-            )
+            } else CombinedInfo.Unavailable
+        )
 
-            Result.success()
-        } catch (e: Exception) {
-            Result.failure()
-        }
+        return Result.success()
     }
 
     private suspend fun setWidgetState(newState: CombinedInfo) {
