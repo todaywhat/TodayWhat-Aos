8480100b4de89a29f7c765f72386bb3dbe433a0b
khs3994 <dev.hyunsss@gmail.com>
2024-12-28 16:31:44 +0900

    [Chore] :: 그림자 추가

--- feature/setting/src/main/java/khs/onmi/setting/component/addwidget/AddWidgetItem.kt
+++ feature/setting/src/main/java/khs/onmi/setting/component/addwidget/AddWidgetItem.kt
@@ -14,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,6 +44,11 @@ fun AddWidgetItem(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
+                modifier = Modifier.shadow(
+                    elevation = 16.dp,
+                    spotColor = Color(0xFF2C2C2C),
+                    ambientColor = Color(0xFF2C2C2C)
+                ),
                 imageVector = ImageVector.vectorResource(previewImage),
                 contentDescription = "add widget item"
             )
