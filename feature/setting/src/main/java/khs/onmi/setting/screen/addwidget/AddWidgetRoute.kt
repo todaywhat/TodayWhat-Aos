b60729e46ae832a31ac808167c1f2fef71f51cd1
khs3994 <dev.hyunsss@gmail.com>
2024-12-28 16:15:12 +0900

    [Add] :: 위젯 리스트에 아이템 연결

--- feature/setting/src/main/java/khs/onmi/setting/screen/addwidget/AddWidgetRoute.kt
+++ feature/setting/src/main/java/khs/onmi/setting/screen/addwidget/AddWidgetRoute.kt
@@ -1,12 +1,11 @@
 package khs.onmi.setting.screen.addwidget
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun AddWidgetRoute(
-    navController: NavController
+    navController: NavController,
 ) {
-    Text("인앱 위젯 추가 페이지")
+    AddWidgetScreen { navController.popBackStack() }
 }
