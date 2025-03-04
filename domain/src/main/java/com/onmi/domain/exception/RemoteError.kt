e3696ed91408143f528c56aeaee064af8d69991a
khs3994 <dev.hyunsss@gmail.com>
2025-03-04 23:35:58 +0900

    [Chore] :: 공용 exception 및 info enum class로 변경

--- domain/src/main/java/com/onmi/domain/exception/RemoteError.kt
+++ domain/src/main/java/com/onmi/domain/exception/RemoteError.kt
@@ -1,51 +1,53 @@
 package com.onmi.domain.exception
 
-sealed interface RemoteError {
-    val code: Int
-    val message: String
+enum class RemoteError(
+    val code: Int,
+    val message: String,
+) {
+    MISSING_REQUIRED_PARAMETER(
+        code = 300,
+        message = "필수 값이 누락되어 있습니다. 요청인자를 참고 하십시오."
+    ),
+    INVALID_REMOTE_KEY(
+        code = 290,
+        message = "인증키가 유효하지 않습니다. 인증키가 없는 경우, 홈페이지에서 인증키를 신청하십시오."
+    ),
+    SERVICE_NOT_FOUND(
+        code = 310,
+        message = "서비스를 찾을 수 없습니다. 요청인자 중 SERVICE를 확인하십시오."
+    ),
+    INVALID_REQUEST_TYPE(
+        code = 333,
+        message = "요청위치 값의 타입이 유효하지 않습니다.요청위치 값은 정수를 입력하세요."
+    ),
+    REQUEST_LIMIT_EXCEEDED(
+        code = 336,
+        message = "데이터요청은 한번에 최대 1,000건을 넘을 수 없습니다."
+    ),
+    DAILY_TRAFFIC_EXCEEDED(
+        code = 337,
+        message = "일별 트래픽 제한을 넘은 실행입니다. 오늘은 더이상 실행할 수 없습니다."
+    ),
+    INTERNAL_SERVER_ERROR(
+        code = 500,
+        message = "서버 오류입니다. 지속적으로 발생시 홈페이지로 문의(Q&A) 바랍니다."
+    ),
+    DATABASE_CONNECTION_ERROR(
+        code = 600,
+        message = "데이터베이스 연결 오류입니다. 지속적으로 발생시 홈페이지로 문의(Q&A) 바랍니다."
+    ),
+    SQL_SYNTAX_ERROR(
+        code = 601,
+        message = "SQL 문장 오류 입니다. 지속적으로 발생시 홈페이지로 문의(Q&A) 바랍니다."
+    ),
+    UNKNOWN_ERROR(
+        code = 999,
+        message = "알 수 없는 에러 발생."
+    );
 
-    data object MissingRequiredParameter : RemoteError {
-        override val code = 300
-        override val message = "필수 값이 누락되어 있습니다. 요청인자를 참고 하십시오."
-    }
-
-    data object InvalidRemoteKey : RemoteError {
-        override val code = 290
-        override val message = "인증키가 유효하지 않습니다. 인증키가 없는 경우, 홈페이지에서 인증키를 신청하십시오."
-    }
-
-    data object ServiceNotFound : RemoteError {
-        override val code = 310
-        override val message = "서비스를 찾을 수 없습니다. 요청인자 중 SERVICE를 확인하십시오."
-    }
-
-    data object InvalidRequestType : RemoteError {
-        override val code = 333
-        override val message = "요청위치 값의 타입이 유효하지 않습니다.요청위치 값은 정수를 입력하세요."
-    }
-
-    data object RequestLimitExceeded : RemoteError {
-        override val code = 336
-        override val message = "데이터요청은 한번에 최대 1,000건을 넘을 수 없습니다."
-    }
-
-    data object DailyTrafficExceeded : RemoteError {
-        override val code = 337
-        override val message = "일별 트래픽 제한을 넘은 실행입니다. 오늘은 더이상 실행할 수 없습니다."
-    }
-
-    data object InternalServerError : RemoteError {
-        override val code = 500
-        override val message = "서버 오류입니다. 지속적으로 발생시 홈페이지로 문의(Q&A) 바랍니다."
-    }
-
-    data object DatabaseConnectionError : RemoteError {
-        override val code = 600
-        override val message = "데이터베이스 연결 오류입니다. 지속적으로 발생시 홈페이지로 문의(Q&A) 바랍니다."
-    }
-
-    data object SQLSyntaxError : RemoteError {
-        override val code = 601
-        override val message = "SQL 문장 오류 입니다. 지속적으로 발생시 홈페이지로 문의(Q&A) 바랍니다."
+    companion object {
+        fun find(code: Int): RemoteError {
+            return entries.find { it.code == code } ?: UNKNOWN_ERROR
+        }
     }
 }
--- domain/src/main/java/com/onmi/domain/info/RemoteInfo.kt
+++ domain/src/main/java/com/onmi/domain/info/RemoteInfo.kt
@@ -1,26 +1,34 @@
 package com.onmi.domain.info
 
-interface RemoteInfo {
-    val code: Int
-    val message: String
+enum class RemoteInfo(
+    val code: Int,
+    val message: String,
+) {
+    SUCCESS(
+        code = 0,
+        message = "정상 처리되었습니다."
+    ),
+    REFERENCE_ONLY(
+        code = 100,
+        message = "(해당 자료는 단순 참고용으로만 활용하시기 바랍니다.)"
+    ),
 
-    data object Success : RemoteInfo {
-        override val code = 0
-        override val message = "정상 처리되었습니다."
-    }
+    DATA_NOT_FOUND(
+        code = 200,
+        message = "해당하는 데이터가 없습니다."
+    ),
+    API_KEY_RESTRICTED(
+        code = 300,
+        message = "관리자에 의해 인증키 사용이 제한되었습니다."
+    ),
+    UNKNOWN_INFO(
+        code = 999,
+        message = "알 수 없는 정보입니다."
+    );
 
-    data object ReferenceOnly : RemoteInfo {
-        override val code = 100
-        override val message = "(해당 자료는 단순 참고용으로만 활용하시기 바랍니다.)"
-    }
-
-    data object DataNotFound : RemoteInfo {
-        override val code = 200
-        override val message = "해당하는 데이터가 없습니다."
-    }
-
-    data object ApiKeyRestricted : RemoteInfo {
-        override val code = 300
-        override val message = "관리자에 의해 인증키 사용이 제한되었습니다."
+    companion object {
+        fun find(code: Int): RemoteInfo {
+            return entries.find { it.code == code } ?: UNKNOWN_INFO
+        }
     }
 }
