a5dbe0b456e31071a4ac8047285891c62315c43d
khs3994 <dev.hyunsss@gmail.com>
2025-03-14 22:59:57 +0900

    [Add] :: 시간표 요구사항에 해당하는 커스텀 exception 추가

--- domain/src/main/java/com/onmi/domain/usecase/timetable/GetTimeTableUseCase.kt
+++ domain/src/main/java/com/onmi/domain/usecase/timetable/GetTimeTableUseCase.kt
@@ -1,33 +1,74 @@
 package com.onmi.domain.usecase.timetable
 
 import com.onmi.database.UserDao
+import com.onmi.domain.exception.NeisException
+import com.onmi.domain.exception.NeisResult
 import com.onmi.domain.repository.TimeTableRepository
 import com.onmi.domain.util.DateUtils
 import kotlinx.coroutines.flow.first
+import java.net.UnknownHostException
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import javax.inject.Inject
 
+sealed interface GetTimeTableException {
+    data object DataEmpty : GetTimeTableException
+    data object TemporaryTimeTable : GetTimeTableException
+    data object InternetDisconnected : GetTimeTableException
+    data class Unknown(val errorCode: String) : GetTimeTableException
+}
+
+sealed interface GetTimeTableState {
+    data class Success(val response: List<String>) : GetTimeTableState
+
+    data class Failure(val exception: GetTimeTableException) : GetTimeTableState
+}
+
 class GetTimeTableUseCase @Inject constructor(
     private val repository: TimeTableRepository,
     private val userDao: UserDao,
 ) {
-    suspend operator fun invoke() = kotlin.runCatching {
-        val userInfo =
-            userDao.getUserInfo().first() ?: throw RuntimeException("fail to get user info")
+    suspend operator fun invoke(): GetTimeTableState {
+        val userInfo = userDao.getUserInfo().first()
+            ?: return GetTimeTableState.Failure(GetTimeTableException.Unknown(NeisResult.UNKNOWN_ERROR.code))
 
-        repository.getTimeTable(
-            schoolCode = userInfo.schoolCode,
-            schoolType = convertSchoolTypeToKey(userInfo.schoolType),
-            educationCode = userInfo.educationCode,
-            grade = userInfo.grade,
-            `class` = userInfo.classroom,
-            department = userInfo.department,
-            date = when {
-                DateUtils.checkIsWeekend() && userInfo.isSkipWeekend -> DateUtils.getNextMondayDate()
-                DateUtils.checkIsAfterDinner() && userInfo.isShowNextDayInfoAfterDinner -> DateUtils.getNextDayDate()
-                else -> convertMillisToDateString(System.currentTimeMillis())
+        val date = when {
+            DateUtils.checkIsWeekend() && userInfo.isSkipWeekend -> DateUtils.getNextMondayDate()
+            DateUtils.checkIsAfterDinner() && userInfo.isShowNextDayInfoAfterDinner -> DateUtils.getNextDayDate()
+            else -> convertMillisToDateString(System.currentTimeMillis())
+        }
+
+        return runCatching {
+            repository.getTimeTable(
+                schoolCode = userInfo.schoolCode,
+                schoolType = convertSchoolTypeToKey(userInfo.schoolType),
+                educationCode = userInfo.educationCode,
+                grade = userInfo.grade,
+                `class` = userInfo.classroom,
+                department = userInfo.department,
+                date = date
+            )
+        }.fold(
+            onSuccess = { result ->
+                GetTimeTableState.Success(result)
+            },
+            onFailure = { exception ->
+                val error = when (exception) {
+                    is NeisException -> when (exception.result) {
+                        NeisResult.DATA_NOT_FOUND -> {
+                            // TODO: 3월임을 구분하는 로직 추가
+                            if (true) GetTimeTableException.TemporaryTimeTable
+                            else GetTimeTableException.DataEmpty
+                        }
+
+                        else -> GetTimeTableException.Unknown(exception.result.code)
+                    }
+
+                    is UnknownHostException -> GetTimeTableException.InternetDisconnected
+                    else -> GetTimeTableException.Unknown(NeisResult.UNKNOWN_ERROR.code)
+                }
+                GetTimeTableState.Failure(error)
             }
         )
     }
