1e85ae6a03a50cd4c748909574231680ed912211
khs3994 <dev.hyunsss@gmail.com>
2025-05-13 21:44:54 +0900

    [Add] :: LocalRepository 추가

--- data/src/main/java/com/onmi/data/dto/option/OptionInfoDto.kt
+++ data/src/main/java/com/onmi/data/dto/option/OptionInfoDto.kt
@@ -1,6 +1,13 @@
 package com.onmi.data.dto.option
 
+import com.onmi.domain.model.option.OptionInfoModel
+
 data class OptionInfoDto(
     var isSkipWeekend: Boolean,
     var isShowNextDayInfoAfterDinner: Boolean,
+)
+
+fun OptionInfoDto.toDomain() = OptionInfoModel(
+    isSkipWeekend = isSkipWeekend,
+    isShowNextDayInfoAfterDinner = isShowNextDayInfoAfterDinner
 )
--- data/src/main/java/com/onmi/data/dto/user/UserInfoDto.kt
+++ data/src/main/java/com/onmi/data/dto/user/UserInfoDto.kt
@@ -1,5 +1,7 @@
 package com.onmi.data.dto.user
 
+import com.onmi.domain.model.user.UserInfoModel
+
 data class UserInfoDto(
     var schoolCode: String,
     var educationCode: String,
@@ -8,4 +10,24 @@ data class UserInfoDto(
     var grade: Int,
     var classroom: Int,
     var department: String,
+)
+
+fun UserInfoDto.toDomain() = UserInfoModel(
+    schoolCode = schoolCode,
+    educationCode = educationCode,
+    schoolName = schoolName,
+    schoolType = schoolType,
+    grade = grade,
+    classroom = classroom,
+    department = department
+)
+
+fun UserInfoModel.toDto() = UserInfoDto(
+    schoolCode = schoolCode,
+    educationCode = educationCode,
+    schoolName = schoolName,
+    schoolType = schoolType,
+    grade = grade,
+    classroom = classroom,
+    department = department
 )
