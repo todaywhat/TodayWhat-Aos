e43834b6ebf42f82b18a2eb03f1df5de55d94e03
khs3994 <dev.hyunsss@gmail.com>
2025-05-22 23:42:28 +0900

    [Chore] :: target date 설정 usecase에서 repository를 이용하도록 수정

--- domain/src/main/java/com/onmi/domain/usecase/common/CalculateTargetDateUseCase.kt
+++ domain/src/main/java/com/onmi/domain/usecase/common/CalculateTargetDateUseCase.kt
@@ -1,23 +1,22 @@
 package com.onmi.domain.usecase.common
 
-import com.onmi.database.option.OptionDao
+import com.onmi.domain.repository.LocalOptionRepository
 import com.onmi.domain.util.DateUtils
 import com.onmi.domain.util.DateUtils.convertMillisToDateString
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-/* TODO("이런 로직도 UseCase가 될 수 있을까?") */
 class CalculateTargetDateUseCase @Inject constructor(
-    private val optionDao: OptionDao,
+    private val optionRepository: LocalOptionRepository,
 ) {
-    suspend operator fun invoke(): Result<String> = kotlin.runCatching {
-        val optionInfo = optionDao.getOptionInfo().first()
-            ?: return@runCatching convertMillisToDateString(System.currentTimeMillis())
-
-        return@runCatching when {
-            DateUtils.checkIsWeekend() && optionInfo.isSkipWeekend -> DateUtils.getNextMondayDate()
-            DateUtils.checkIsAfterDinner() && optionInfo.isShowNextDayInfoAfterDinner -> DateUtils.getNextDayDate()
-            else -> convertMillisToDateString(System.currentTimeMillis())
+    operator fun invoke(): Flow<String> {
+        return optionRepository.getOptionInfo().map { optionInfo ->
+            when {
+                DateUtils.checkIsWeekend() && optionInfo.isSkipWeekend -> DateUtils.getNextMondayDate()
+                DateUtils.checkIsAfterDinner() && optionInfo.isShowNextDayInfoAfterDinner -> DateUtils.getNextDayDate()
+                else -> convertMillisToDateString(System.currentTimeMillis())
+            }
         }
     }
 }
