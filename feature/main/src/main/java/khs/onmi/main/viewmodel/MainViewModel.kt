e12bf311044e376277f41aa7d392dd0576090f59
khs3994 <dev.hyunsss@gmail.com>
2025-07-23 21:05:45 +0900

    [Chore] :: orbit 버전업에 따른 viewmodel 변경사항 적용

--- feature/main/src/main/java/khs/onmi/main/viewmodel/MainViewModel.kt
+++ feature/main/src/main/java/khs/onmi/main/viewmodel/MainViewModel.kt
@@ -16,9 +16,6 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
-import org.orbitmvi.orbit.syntax.simple.intent
-import org.orbitmvi.orbit.syntax.simple.postSideEffect
-import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
